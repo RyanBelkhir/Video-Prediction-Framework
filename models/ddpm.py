c0ddd53527059d82e3886c2c50d7ec6d99cb4573
RyanBelkhir <ryan.belkhir@gmail.com>
2022-06-07 14:12:36 +0800

    added Unet and DDPM

---
+++ models/ddpm.py
@@ -0,0 +1,96 @@
+import torch
+import numpy as np
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+n_steps = 1000
+
+def make_beta_schedule(schedule='linear', n_timesteps=1000, start=0.02, end=0.0001):
+    """
+    Create a schedule of betas
+    """
+    if schedule == 'linear':
+        betas = torch.linspace(start, end, n_timesteps)
+    elif schedule == 'geometric':
+        betas = torch.logspace(np.log10(start), np.log10(end), n_timesteps)
+    elif schedule == "quad":
+        betas = torch.linspace(start ** 0.5, end ** 0.5, n_timesteps) ** 2
+    elif schedule == "sigmoid":
+        betas = torch.linspace(-6, 6, n_timesteps)
+        betas = torch.sigmoid(betas) * (end - start) + start
+    return betas
+
+def get_alphas(schedule="geometric", n_timesteps=1000, start=0.02, end=0.0001):
+    betas = make_beta_schedule(schedule, n_timesteps, start, end).to(device)
+    alphas = 1 - betas
+    alphas_prod = torch.cumprod(1 - betas.flip(0), 0).flip(0).to(device)
+    alphas_prod_p = torch.cat(([alphas_prod[1:], torch.tensor([1.0]).to(device)])).to(device)
+    alphas_bar_sqrt = alphas_prod_p.sqrt().to(device)
+    one_minus_alphas_bar_log = torch.log(1 - alphas_prod).to(device)
+    one_minus_alphas_bar_sqrt = torch.sqrt(1 - alphas_prod).to(device)
+
+    return betas, alphas, alphas_prod, alphas_prod_p, alphas_bar_sqrt, one_minus_alphas_bar_log, one_minus_alphas_bar_sqrt
+
+def get_posteriors(betas, alphas, alphas_prod, alphas_prod_p):
+    posterior_mean_coef_1 = (betas * torch.sqrt(alphas_prod_p) / (1 - alphas_prod))
+    posterior_mean_coef_2 = ((1 - alphas_prod_p) * torch.sqrt(alphas) / (1 - alphas_prod))
+    posterior_variance = betas * (1 - alphas_prod_p) / (1 - alphas_prod)
+    posterior_log_variance_clipped = torch.log(torch.cat((posterior_variance[1].view(1, 1), posterior_variance[1:].view(-1, 1)), 0)).view(-1)
+    return posterior_mean_coef_1, posterior_mean_coef_2, posterior_variance, posterior_log_variance_clipped
+
+class DDPM(object):
+
+    def __init__(self, schedule="geometric", n_timesteps=1000, start=0.02, end=0.0001):
+        self.betas, self.alphas, self.alphas_prod, self.alphas_prod_p, self.alphas_bar_sqrt, self.one_minus_alphas_bar_log, self.one_minus_alphas_bar_sqrt = get_alphas(schedule, n_timesteps, start, end)
+        self.posterior_mean_coef_1, self.posterior_mean_coef_2, self.posterior_variance, self.posterior_log_variance_clipped = get_posteriors(self.betas, self.alphas, self.alphas_prod, self.alphas_prod_p)
+    
+    def extract(self, input, t, x):
+        shape = x.shape
+        out = torch.gather(input, 0, t.to(input.device))
+        reshape = [t.shape[0]] + [1] * (len(shape) - 1)
+        return out.reshape(*reshape)
+
+    def q_sample(self, x_0, t, noise=None):
+        if noise is None:
+            noise = torch.randn_like(x_0).cuda()
+        alphas_t = self.extract(self.alphas_bar_sqrt, t, x_0)
+        alphas_1_m_t = self.extract(self.one_minus_alphas_bar_sqrt, t, x_0)
+        return (alphas_t * x_0 + alphas_1_m_t * noise)
+
+    def q_posterior_mean_variance(self, x_0, x_t, t):
+        coef_1 = self.extract(self.posterior_mean_coef_1, t, x_0)
+        coef_2 = self.extract(self.posterior_mean_coef_2, t, x_0)
+        mean = coef_1 * x_0 + coef_2 * x_t
+        var = self.extract(self.posterior_log_variance_clipped, t, x_0)
+        return mean, var
+
+class EMA(object):
+    def __init__(self, mu=0.999):
+        self.mu = mu
+        self.shadow = {}
+
+    def register(self, module):
+        for name, param in module.named_parameters():
+            if param.requires_grad:
+                self.shadow[name] = param.data.clone()
+
+    def update(self, module):
+        for name, param in module.named_parameters():
+            if param.requires_grad:
+                self.shadow[name].data = (1. - self.mu) * param.data + self.mu * self.shadow[name].data
+
+    def ema(self, module):
+        for name, param in module.named_parameters():
+            if param.requires_grad:
+                param.data.copy_(self.shadow[name].data)
+
+    def ema_copy(self, module):
+        module_copy = type(module)(module.config).to(module.config.device)
+        module_copy.load_state_dict(module.state_dict())
+        self.ema(module_copy)
+        return module_copy
+
+    def state_dict(self):
+        return self.shadow
+
+    def load_state_dict(self, state_dict):
+        self.shadow = state_dict
--- test.py
+++ test.py
@@ -40,38 +40,6 @@ for seq, seq_target in train_loader:
     print('Target: ', seq_target.shape)
     break
 
-from video_diffusion_pytorch.video_diffusion_pytorch import Unet3D, GaussianDiffusion, Trainer
-
-model = Unet3D(
-    dim = 64,
-    dim_mults = (1, 2, 4, 8),
-)
-
-diffusion = GaussianDiffusion(
-    model,
-    image_size = 64,
-    num_frames = 10,
-    timesteps = 1000,   # number of steps
-    loss_type = 'l1'    # L1 or L2
-).cuda()
-
-trainer = Trainer(
-    diffusion,
-    train_loader,                         # this folder path needs to contain all your training data, as .gif files, of correct image size and number of frames
-    train_batch_size = 32,
-    train_lr = 1e-4,
-    save_and_sample_every = 1000,
-    train_num_steps = 700000,         # total training steps
-    gradient_accumulate_every = 2,    # gradient accumulation steps
-    ema_decay = 0.995,                # exponential moving average decay
-    amp = True                        # turn on mixed precision
-)
-
-trainer.train()
-
-
-
-
 """import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import matplotlib.animation as animation
@@ -87,3 +55,8 @@ ani = animation.ArtistAnimation(fig, frames, interval=50, blit=True,
                                 repeat_delay=100)
 # ani.save('movie.mp4')
 plt.show()"""
+
+from models.models import UNet
+
+model = UNet()
+print("Model created.")
